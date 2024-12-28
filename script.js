15861c87ecfd9e925bda1b4e42a18af83957eb33
SK <115445955+san97Lonza@users.noreply.github.com>
2024-12-28 20:59:17 +0800

    Created the function playRound that will play the game for one round and determine the winner.
    Created the function playGame that will play the
    game for 5 rounds by calling the playRound function
    5 times.
    
    Tally up the scores after each round and determine
    the winner using the checkScore function.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-function getHumanChoice() {
+function gethumanResult() {
     let humanInput = prompt("Pls choose: Scissors, Paper or Rock!").toLowerCase();
+
     return humanInput;
 }
 
@@ -13,6 +14,60 @@ function getComputerChoice() {
     } else {
         computerInput = "rock";
     }
-    console.log(choice);
     return computerInput;
 }
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+
+        let computerResult = computerChoice();
+        let humanResult = humanChoice();
+
+        console.log(`Your choice is ${humanResult}`);
+        console.log(`The computer choice is ${computerResult}`);
+
+
+        if (humanResult === computerResult) {
+            console.log(`Its a draw! ${humanResult} equal to ${computerResult}!`);
+            console.log(`Your score is ${humanScore}\nThe Computer score is ${computerScore}`);
+            return;
+        } else if ((humanResult == "scissors" && computerResult == "paper") || (humanResult == "paper" && computerResult == "rock") || (humanResult == "rock" && computerResult == "scissors")) {
+            humanScore += 1;
+            console.log(`You win this round! ${humanResult} beats ${computerResult}!`);
+            console.log(`Your score is ${humanScore}\nThe Computer score is ${computerScore}`);
+            return;
+        } else {
+            computerScore += 1;
+            console.log(`You lose this round! ${computerResult} beats ${humanResult}!`);
+            console.log(`Your score is ${humanScore}\nThe Computer score is ${computerScore}`);
+            return;
+        }
+    }
+
+    function checkScore() {
+        if (humanScore === computerScore) {
+            console.log(`Your score is ${humanScore}\nThe Computer score is ${computerScore}\nIt is a tie`);
+            return;
+        } else if (humanScore > computerScore) {
+            console.log(`You won! Your total score is ${humanScore}\nThe Computer total score is ${computerScore}\n`);
+            return;
+
+        } else {
+            console.log(`The Computer won! Your total score is ${humanScore}\nThe Computer total score is ${computerScore}\n`);
+            return;
+        }
+    }
+
+    playRound(gethumanResult, getComputerChoice);
+    playRound(gethumanResult, getComputerChoice);
+    playRound(gethumanResult, getComputerChoice);
+    playRound(gethumanResult, getComputerChoice);
+    playRound(gethumanResult, getComputerChoice);
+    checkScore();
+    return;
+}
+
+playGame();
